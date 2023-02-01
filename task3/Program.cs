65e77c90ba4e40fd49da12b45d176a9300650ec9
Евгений <Grekos_89@mail.ru>
2023-02-01 20:21:39 +0300

    фибоничи

---
+++ task3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Не используя рекурсию, выведите первые N чисел Фибоначчи.
+// Первые два числа Фибоначчи: 0 и 1.
+//Если N = 5 -> 0 1 1 2 3
+//Если N = 3 -> 0 1 1
+//Если N = 7 -> 0 1 1 2 3 5 8
+PrintFibNum(7);
+void PrintFibNum(int N)
+{
+    int firstNum = 0;
+    int secondNum = 1;
+    if(N == 1)
+    {
+        System.Console.WriteLine(firstNum);
+        return;
+    }
+    
+    System.Console.WriteLine(firstNum);
+    System.Console.WriteLine(secondNum);
+    for(int i = 3; i <= N; i++)
+    {
+        int curentNum = firstNum + secondNum;
+        System.Console.WriteLine(curentNum);
+        firstNum = secondNum;
+        secondNum = curentNum;  
+
+    }
+}
