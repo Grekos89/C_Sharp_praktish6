056aeb3606fd08b2ad64ba4889d634741a5fe2af
Евгений <Grekos_89@mail.ru>
2023-02-01 20:22:03 +0300

    поэтапное копирование элементов массива

---
+++ task4/Program.cs
@@ -0,0 +1,46 @@
+﻿// Напишите программу, которая будет создавать 
+//копию заданного массива с помощью поэлементного копирования.
+int[] arr = RandomArray(10);
+PrintArray(arr);
+int[] arr2 = CopyArray(arr);
+PrintArray(arr2);
+
+int[] CopyArray(int[] arr)
+{
+    int[] newArr = new int[arr.Length];
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        newArr[i] = arr[i];
+    }
+    return newArr;
+}
+    
+
+
+
+
+
+
+
+
+
+int[] RandomArray(int length)
+{
+     int[]arr = new int[length];
+     for(int i = 0; i < length; i++)
+     {
+         arr[i] = new Random().Next(1,100);
+     }
+     return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    
+    foreach(int item in arr)
+    {
+        System.Console.Write($"{item}, ");
+    } 
+    System.Console.WriteLine();
+}
